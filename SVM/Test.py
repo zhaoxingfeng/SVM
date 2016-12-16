9b9ea5b63f23b04eabeec95fac70a39e77f09413
zhaoxingfeng <1250583734@qq.com>
2016-12-16 21:46:59 +0800

    Update Test.py

--- SVM/Test.py
+++ SVM/Test.py
@@ -8,6 +8,7 @@
 from __future__ import division
 import numpy as np
 from SVM import *
+from sklearn.cross_validation import train_test_split
 import sys
 if sys.version_info[0] >= 3:
     xrange = range
@@ -25,27 +26,16 @@ def wine():
                 else:
                     labelMat.append(-1)
         return np.mat(dataMat), np.mat(labelMat).T
+
     print("Step1: Loading data......")
     filename = r'wine.txt'
     dataMat, labelMat = loadDataSet(filename)
-    train_x1 = dataMat[0:41]
-    train_x2 = dataMat[60:110]
-    train_x = np.vstack((train_x1, train_x2))
-    train_y1 = labelMat[0:41]
-    train_y2 = labelMat[60:110]
-    train_y = np.vstack((train_y1, train_y2))
-
-    test_x1 = dataMat[41:59]
-    test_x2 = dataMat[110:]
-    test_x = np.vstack((test_x1, test_x2))
-    test_y1 = labelMat[41:59]
-    test_y2 = labelMat[110:]
-    test_y = np.vstack((test_y1, test_y2))
+    train_x, test_x, train_y, test_y = train_test_split(dataMat, labelMat, test_size = 0.2)
 
     print("Step2: Training SVM......")
-    maxIter = 50
-    C = 10.6
+    C = 20
     toler = 0.001
+    maxIter = 50
     kernelOption = ("rbf", 20)
     svmClassifier = trainSVM(train_x, train_y, C, toler, maxIter, kernelOption)
 
@@ -53,6 +43,8 @@ def wine():
     accuracy, labelpredict, num = testSVM(svmClassifier, test_x, test_y)
     print("\tAccuracy = %.2f%%" % (accuracy * 100))
 wine()
+
+print("------------------------------------------------------------------")
 
 # 手写字digits, 0/1/2
 def handwriting():
@@ -75,13 +67,17 @@ def handwriting():
             labelMat.append(num)
             dataMat[i, :] = vector('%s\%s' % (dirName, fileList[i]))
         return np.mat(dataMat), np.mat(labelMat).T
+
     print("Step1: Loading data......")
     traindataMat, trainlabelMat = loadfiles(r"trainingDigits")
     testdataMat, testlabelMat = loadfiles(r"testDigits")
 
     print("Step2: Training SVM......")
-    # 第一个分类器：0 -> 12
-    # 训练样本
+    C = 200
+    toler = 0.0001
+    maxIter = 100
+    kernelOption = ("rbf", 10)
+    # 第一个分类器：0 / 12
     trainlabel012 = []
     for i in range(len(trainlabelMat)):
         if trainlabelMat[i] == 0:
@@ -90,26 +86,8 @@ def handwriting():
             trainlabel012.append(1)
     trainlabel012Mat = np.mat(trainlabel012).T
     traindata012Mat = traindataMat[::]
-    # 测试样本
-    testlabel012 = []
-    for i in range(len(testlabelMat)):
-        if testlabelMat[i] == 0:
-            testlabel012.append(-1)
-        else:
-            testlabel012.append(1)
-    testlabel012Mat = np.mat(testlabel012).T
-    testdata012Mat = testdataMat[::]
-
-    maxIter = 100
-    C = 200
-    toler = 0.0001
-    kernelOption = ("rbf", 10)
     svmClassifier012 = trainSVM(traindata012Mat, trainlabel012Mat, C, toler, maxIter, kernelOption)
-    accuracy012, labelpredict012, numright012 = testSVM(svmClassifier012, testdata012Mat, testlabel012Mat)
-    print("\tAccuracy012 = %.2f%%" % (accuracy012 * 100))
-
-    # 第二个分类器：1 -> 2
-    # 训练样本
+    # 第二个分类器：1 / 2
     trainlabel12 = []
     for i in range(trainlabel012.count(-1),len(trainlabelMat)):
         if trainlabelMat[i] == 1:
@@ -118,12 +96,25 @@ def handwriting():
             trainlabel12.append(1)
     trainlabel12Mat = np.mat(trainlabel12).T
     traindata12Mat = traindataMat[trainlabel012.count(-1):]
-    # 测试样本，被前一个分类器分为 1 且不包含本来是 -1 结果被分类为 1 的样本
+    svmClassifier12 = trainSVM(traindata12Mat, trainlabel12Mat, C, toler, maxIter, kernelOption)
+
+    print("Step3: Testing classifier......")
+    # 测试样本 0 / 12
+    testlabel012 = []
+    for i in range(len(testlabelMat)):
+        if testlabelMat[i] == 0:
+            testlabel012.append(-1)
+        else:
+            testlabel012.append(1)
+    testlabel012Mat = np.mat(testlabel012).T
+    testdata012Mat = testdataMat[::]
+    accuracy012, labelpredict012, numright012 = testSVM(svmClassifier012, testdata012Mat, testlabel012Mat)
+    # 测试样本 1 / 2
+    # 被前一个分类器分为 1 且不包含本来是 -1 结果被分类为 1 的样本
     testlabel12index = []
     for i in range(len(labelpredict012)):
         if labelpredict012[i] == 1 and testlabel012Mat[i] == 1:
             testlabel12index.append(i)
-
     num12 = len(testlabel12index)
     testlabel12raw = testlabelMat[testlabel12index]
     testlabel12 = [0 for i in range(num12)]
@@ -134,11 +125,11 @@ def handwriting():
             testlabel12[i] = 1
     testlabel12Mat = np.mat(testlabel12).T
     testdata12Mat = testdataMat[testlabel12index]
-
-    svmClassifier12 = trainSVM(traindata12Mat, trainlabel12Mat, C, toler, maxIter, kernelOption)
     accuracy12, labelpredict12, numright12 = testSVM(svmClassifier12, testdata12Mat, testlabel12Mat)
-    print("\tAccuracy12 = %.2f%%" % (accuracy12 * 100))
-
+    # 统计被正确分类的样本数量，包括：
+    # 1、第一轮分类 -> 本来是-1结果被分为-1的个数。
+    #    第一轮分类结束后，本来是1结果被分类为1的样本进入第二轮分类；本来是-1结果本分类为1的样本已经分类错误，不参与第二轮分类
+    # 2、第二轮分类 -> -1、1被正确分类的个数。
     total_accuracy = (numright012 + num12 * accuracy12) / len(testlabelMat)
-    print("Final accuracy = %.3f%%" % (total_accuracy * 100))
+    print("\tFinal accuracy = %.3f%%" % (total_accuracy * 100))
 handwriting()
